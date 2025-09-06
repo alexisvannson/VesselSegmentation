2be42e96f3a628336bee3adbba9ebd62d7b6b53a
alexisvannson <vannson.alexis@gmail.com>
2025-09-07 01:19:23 +0200

    change to Dice loss and implemented patience

--- train.py
+++ train.py
@@ -1,28 +1,32 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
+from torchmetrics.classification import Dice
+import tqdm
 
 def train_model(model, dataloader, epochs: int, output_dir: str, patience =5):
     optimizer = optim.Adam(model.parameters(), lr=0.001) #why model params ?
-    criterion = nn.CrossEntropyLoss() # have to change that with prooper segmentation loss
-    previous_loss = float('inf')
+    criterion = Dice(average='micro') #have to check if this is the proper setup
+    best_loss = float('inf')
     for i in range(epochs):
         loss_aggregation = 0
-        for image, label in dataloader:
+        for image, label in tqdm(dataloader):
             logits = model(image)
             loss = criterion(logits, label)
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-            loss_aggregation += loss
+            loss_aggregation += loss # why loss.item()????
         avrg_loss = loss_aggregation / len(dataloader)
-        if avrg_loss < previous_loss:
-            previous_loss = avrg_loss
-            patience = patience
+        
+        if avrg_loss < best_loss:
+            best_loss = avrg_loss
+            patience_counter = 0
         else:
-            patience -= 1
-        if patience == 0:
+            patience_counter += 1
+        if patience_counter > patience:
+            print(f'Final average loss of epoch {i + 1} is {avrg_loss}')
             torch.save(model.state_dict(), output_dir)
-            return
+            break
         print(f'average loss of epoch {i + 1} is {avrg_loss}')
         torch.save(model.state_dict(), output_dir)
