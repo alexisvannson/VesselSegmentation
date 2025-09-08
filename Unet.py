0f23a11e6fb297590f00db9db5f0666f11112763
alexisvannson <vannson.alexis@gmail.com>
2025-09-08 22:59:21 +0200

    best Unet architecture my brain could come up with

--- Unet.py
+++ Unet.py
@@ -1,18 +1,47 @@
 import torch.nn as nn
+import torch
 
 class MyGoatedUnet(nn.Module):
-    def __init__(self, input_size):    # change conv2d to conv 3d  (oops)
-        super().__init__()            # n_out = (500 + (2* 0) - 2) / 2
-        # check how conv3d works
+    def __init__(self, input_size):
+        super().__init__()           
         self.input_size = input_size
-        self.down_block1 = [nn.Conv3d(in_channels=1, out_channels=64, kernel_size=2, padding=0, stride=1), nn.Conv3d(in_channels=64, out_channels=64, kernel_size=2, padding=0, stride=1), nn.Conv3d(in_channels=64, out_channels=64, kernel_size=2, padding=0, stride=1)]
-        self.down_block2 = [nn.Conv3d(in_channels=64, out_channels=128, kernel_size=2, padding=0, stride=1), nn.Conv3d(in_channels=128, out_channels=128, kernel_size=2, padding=0, stride=1), nn.Conv3d(in_channels=128, out_channels=128, kernel_size=2, padding=0, stride=1)]
-        self.down_block3 = [nn.Conv3d(in_channels=128, out_channels=256, kernel_size=2, padding=0, stride=1), nn.Conv3d(in_channels=256, out_channels=256, kernel_size=2, padding=0, stride=1), nn.Conv3d(in_channels=256, out_channels=256, kernel_size=2, padding=0, stride=1)]
-        self.down_block4 = [nn.Conv3d(in_channels=256, out_channels=512, kernel_size=2, padding=0, stride=1), nn.Conv3d(in_channels=512, out_channels=512, kernel_size=2, padding=0, stride=1), nn.Conv3d(in_channels=512, out_channels=512, kernel_size=2, padding=0, stride=1)]
-        #self.down = nn.Sequential([self.down_block1 + [self.maxpool] + self.down_block2 + [self.maxpool] + self.down_block3 + [self.maxpool] + self.down_block4])
         self.maxpool = nn.MaxPool2d(2,2)
-    
+        self.relu = nn.ReLU()
+        
+        self.down_block1 = nn.Sequential([nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=0, stride=1), self.relu(), 
+                                          nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=0, stride=1), self.relu()])
+        
+        self.down_block2 = nn.Sequential([nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=0, stride=1), self.relu(),
+                                          nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=0, stride=1), self.relu()])
+        
+        self.down_block3 = nn.Sequential([nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=0, stride=1), self.relu(),
+                                          nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=0, stride=1), self.relu()])
+        
+        self.down_block4 = nn.Sequential([nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=0, stride=1), self.relu(),
+                                          nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=0, stride=1), self.relu()])
+        
+        self.upscale1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=3, padding=0, stride=1)
+        
+        self.up_block1 = nn.Sequential([nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=0, stride=1), self.relu(),
+                                        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=0, stride=1), self.relu()])
+        
+        self.upscale2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, padding=0, stride=1)
+        
+        self.up_block2 = nn.Sequential([nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=0, stride=1), self.relu(),
+                                        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=0, stride=1), self.relu()])
+        
+        self.upscale3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, padding=0, stride=1)
+        
+        self.up_block3 = nn.Sequential([nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=0, stride=1), self.relu(),
+                                        nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=0, stride=1), self.relu()])
+        
+        self.upscale4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, padding=0, stride=1)
+        
+        self.up_block4 = nn.Sequential([nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=0, stride=1), self.relu(),
+                                        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=0, stride=1), self.relu(),
+                                        nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1, padding=0, stride=1)])
     def forward(self, x):
+        # Encoder
         output_block1 = self.down_block1(x)
         input_block2 = self.maxpool(output_block1)
         
@@ -24,5 +53,17 @@ class MyGoatedUnet(nn.Module):
         
         output_final_block = self.down_block4(input_block4)
         
-        x = self.up(x)
-        return x
+        # Decoder
+        input_block_up1 = torch.concat((input_block4, self.upscale1(output_final_block)), dim=1)
+        output_block_up_1 = self.up_block1(input_block_up1)
+        
+        input_block_up2 = torch.concat((input_block3, self.upscale2(output_block_up_1)), dim=1)
+        output_block_up_2 = self.up_block2(input_block_up2)
+        
+        input_block_up3 = torch.concat((input_block3, self.upscale3(output_block_up_2)), dim=1)
+        output_block_up_3 = self.up_block3(input_block_up3)
+        
+        input_block_up4 = torch.concat((input_block3, self.upscale4(output_block_up_2)), dim=1)
+        output_block_up_4 = self.up_block4(input_block_up4)
+        
+        return output_block_up_4
