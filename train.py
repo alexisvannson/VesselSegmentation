87f198a29eec992332bb9850347a783dabb938ce
alexisvannson <vannson.alexis@gmail.com>
2025-09-08 18:37:27 +0200

    setup basic dataset management

--- train.py
+++ train.py
@@ -1,23 +1,37 @@
+# Python imports
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from torchmetrics.classification import Dice
+import torchmetrics.classification  
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 import tqdm
 
-def train_model(model, dataloader, epochs: int, output_dir: str, patience =5):
+#Module imports
+import Unet
+
+def load_dataset(resize_value=128, dataset_path='trainnig_data'):
+    transform = transforms.Compose([transforms.Resize(resize_value, resize_value),
+                                    transforms.ToTensor()])
+    dataset = datasets.ImageFolder(dataset_path, transform= transform)  #get library for image folder
+    return dataset
+
+
+
+def train_model(model, dataset, epochs: int, output_dir: str, patience=5):
     optimizer = optim.Adam(model.parameters(), lr=0.001) #why model params ?
-    criterion = Dice(average='micro') #have to check if this is the proper setup
+    criterion = torchmetrics.classification.Dice(average='micro') #have to check if this is the proper setup
     best_loss = float('inf')
     for i in range(epochs):
         loss_aggregation = 0
-        for image, label in tqdm(dataloader):
+        for image, label in tqdm(dataset):
             logits = model(image)
             loss = criterion(logits, label)
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
             loss_aggregation += loss # why loss.item()????
-        avrg_loss = loss_aggregation / len(dataloader)
+        avrg_loss = loss_aggregation / len(dataset)
         
         if avrg_loss < best_loss:
             best_loss = avrg_loss
@@ -30,3 +44,9 @@ def train_model(model, dataloader, epochs: int, output_dir: str, patience =5):
             break
         print(f'average loss of epoch {i + 1} is {avrg_loss}')
         torch.save(model.state_dict(), output_dir)
+        
+        
+if __name__ == "__main__":
+    dataset = load_dataset()
+    model = Unet.MyGoatedUnet()
+    train_model(model, dataset,)
