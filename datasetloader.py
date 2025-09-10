d50a7a0fc3e05babc514faae6fb44ea6cf2c92a3
alexisvannson <vannson.alexis@gmail.com>
2025-09-10 12:30:08 +0200

    changed it to fit the segmentation task (got rid of ImageFolder which created labels which are only useful for classification tasks and put Dataloader in the training function)

--- datasetloader.py
+++ datasetloader.py
@@ -1,23 +1,38 @@
 # Python imports
 from torch.utils.data import Dataset
-import torchvision.datasets as datasets
 import torchvision.transforms as transforms
-import torch
 import torch.utils.data  as dataloader
+import os
+import PIL
 
-
-class Datasetloader(Dataset):
-    def __init__(self, dataset_path="training_data", resize_value=572):
+class SegmentationDataset(Dataset):
+    def __init__(self, images_path, label_path, resize_value=572):
         super().__init__()
-        self.dataset_path = dataset_path
-        self.transform = transforms.Compose([transforms.Resize((resize_value, resize_value)),
-                                             transforms.ToTensor(),
-                                             transforms.Grayscale()])
-        self.dataset = datasets.ImageFolder(dataset_path, transform=self.transform)
-        self.dataloader = dataloader.DataLoader(self.dataset, batch_size=8, shuffle=True)
-    def __len__(self):
-        return len(self.dataset)
+        self.images_path = images_path
+        self.label_path = label_path
+        self.img_transform = transforms.Compose([transforms.Resize((resize_value, resize_value)),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize(mean=[0.5], std=[0.5])])
+        
+        self.label_path = transforms.Compose([transforms.Resize((resize_value, resize_value)),
+                                            transforms.ToTensor(),
+                                            transforms.Grayscale()])
+        
+        self.images = sorted(os.listdir(images_path))
+        self.label = sorted(os.listdir(label_path))
     
+    def __len__(self):
+        return len(self.images)
+
     def __getitem__(self, idx):
-        image, label = self.dataset[idx]
-        return image, torch.tensor(label)
+        image = PIL.Image.open(os.path.join(self.images_path , self.images[idx])).convert('RGB')  
+        label = PIL.Image.open(os.path.join(self.label_path , self.label[idx])).convert('L')
+        
+        image = self.img_transform(image)
+        label = self.label_transform(label)
+        return image, label
+
+
+
+dataset = SegmentationDataset(images_path='images', label_path='label')
+dataloader = dataloader.DataLoader(dataset, batch_size=8, shuffle=True)
