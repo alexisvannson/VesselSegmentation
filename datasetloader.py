5295022177e43fb1e18109f631f3ae33b8ca22f1
alexisvannson <vannson.alexis@gmail.com>
2025-09-11 11:32:27 +0200

    finally working datasetloader: we use os.listdir to get the paths, sort them, then open them and transform them in the getitem

--- datasetloader.py
+++ datasetloader.py
@@ -4,6 +4,7 @@ import torchvision.transforms as transforms
 import torch.utils.data  as dataloader
 import os
 import PIL
+import glob
 
 class SegmentationDataset(Dataset):
     def __init__(self, images_path, label_path, resize_value=572):
@@ -14,25 +15,38 @@ class SegmentationDataset(Dataset):
                                                 transforms.ToTensor(),
                                                 transforms.Normalize(mean=[0.5], std=[0.5])])
         
-        self.label_path = transforms.Compose([transforms.Resize((resize_value, resize_value)),
+        self.label_transform = transforms.Compose([transforms.Resize((resize_value, resize_value)),
                                             transforms.ToTensor(),
                                             transforms.Grayscale()])
         
         self.images = sorted(os.listdir(images_path))
-        self.label = sorted(os.listdir(label_path))
-    
+        self.labels = sorted(os.listdir(label_path))
+
     def __len__(self):
+        assert len(self.images) == len(self.labels), "Number of images and labels should be the same"
         return len(self.images)
 
     def __getitem__(self, idx):
-        image = PIL.Image.open(os.path.join(self.images_path , self.images[idx])).convert('RGB')  
-        label = PIL.Image.open(os.path.join(self.label_path , self.label[idx])).convert('L')
-        
+        image_path = os.path.join(self.images_path, self.images[idx])
+        print(image_path)
+        label_path = os.path.join(self.label_path, self.labels[idx])
+        print(label_path)
+        print(os.path.basename(image_path))
+        print(os.path.basename(label_path))
+        image = PIL.Image.open(image_path)
+        label = PIL.Image.open(label_path)
         image = self.img_transform(image)
         label = self.label_transform(label)
         return image, label
 
 
 
-dataset = SegmentationDataset(images_path='images', label_path='label')
-dataloader = dataloader.DataLoader(dataset, batch_size=8, shuffle=True)
+
+if __name__ == "__main__":
+    dataset = SegmentationDataset(images_path='/Users/philippevannson/Desktop/ongoing_stuff/VesselSegmentation/training_data/images', label_path='/Users/philippevannson/Desktop/ongoing_stuff/VesselSegmentation/training_data/1st_manual')
+    print(dataset.__len__())
+    dataloader = dataloader.DataLoader(dataset, batch_size=8, shuffle=True)
+    for image, label in dataloader:
+        print(image.shape)
+        print(label.shape)
+        break
